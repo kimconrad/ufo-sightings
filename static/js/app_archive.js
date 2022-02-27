9fec6ad04e276e7bce3cf488773630d716fbe6d9
Kimberly Conrad <kimberlyconrad@Kimberlys-MacBook-Air.local>
2022-02-26 20:45:49 -0800

    updating index.html

--- static/js/app_archive.js
+++ static/js/app_archive.js
@@ -1,10 +1,48 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+// // Simple JavaScript console.log statement
+// function printHello() {
+//   console.log("Hello there!");
+// }
+
+// // Functions can call other functions
+// function doubleAddition(c, d) {
+//   var total = addition(c, d) * 2;
+//   return total;
+// }
+
+// printHello = () => "Hello there!";
+
+// let friends = ["Sarah", "Greg", "Cindy", "Jeff"];
+
+// function listLoop(userList) {
+//   for (var i = 0; i < userList.length; i++) {
+//     console.log(userList[i]);
+//   }
+// }
+// let vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
+// for (var i = 0; i < vegetables.length; i++) {
+// }
+
+// var vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
+
+// for (var i = 0; i < vegetables.length; i++) {
+//     console.log("I love " + vegetables[i]);
+// }
+// for (var i = 0; i < 5; i++) {
+//   console.log("I am " + i);
+// }
+
 // build empty table
+// data is variable holding array (object)
 
-function buildTable(data) {
-    tbody.html("");
-  }
-
-
+// function buildTable(data) {
+//     tbody.html("");
+//   }
 
 function buildTable(data) {
   // First, clear out any existing data
@@ -15,7 +53,6 @@ function buildTable(data) {
   data.forEach((dataRow) => {
     // Append a row to the table body
     let row = tbody.append("tr");
-
     // Loop through each field in the dataRow and add
     // each value as a table cell (td)
     Object.values(dataRow).forEach((val) => {
@@ -27,53 +64,53 @@ function buildTable(data) {
 }
 
 
-// handle what to do with input given
-function handleClick() {
-    let date = d3.select("#datetime").property("value");
-    // default filter is table data to let user set own search terms
-    let filteredData = tableData;
+// // handle what to do with input given
+// function handleClick() {
+//     let date = d3.select("#datetime").property("value");
+//     // default filter is table data to let user set own search terms
+//     let filteredData = tableData;
 
 
-// if-statement syntax
-if ( condition ) { code to execute }
+// // if-statement syntax
+// if ( condition ) { code to execute }
 
-// pseudocode practice
-if (a date is entered) {
-    Filter the default data to show only the date entered
-};
+// // pseudocode practice
+// if (a date is entered) {
+//     Filter the default data to show only the date entered
+// };
 
-if (date) {
-    filteredData = filteredData.filter(row => row.datetime === date);
-};
+// if (date) {
+//     filteredData = filteredData.filter(row => row.datetime === date);
+// };
 
 // === exact match
 // == loose match
 
 
-//complete function
-function handleClick() {
-    // Grab the datetime value from the filter
-    let date = d3.select("#datetime").property("value");
-    let filteredData = tableData;
+// //complete function
+// function handleClick() {
+//     // Grab the datetime value from the filter
+//     let date = d3.select("#datetime").property("value");
+//     let filteredData = tableData;
   
-     // Check to see if a date was entered and filter the
-    // data using that date.
-    if (date) {
-      // Apply `filter` to the table data to only keep the
-      // rows where the `datetime` value matches the filter value
-      filteredData = filteredData.filter(row => row.datetime === date);
-    };
+//      // Check to see if a date was entered and filter the
+//     // data using that date.
+//     if (date) {
+//       // Apply `filter` to the table data to only keep the
+//       // rows where the `datetime` value matches the filter value
+//       filteredData = filteredData.filter(row => row.datetime === date);
+//     };
   
-     // Rebuild the table using the filtered data
-    // @NOTE: If no date was entered, then filteredData will
-    // just be the original tableData.
-    buildTable(filteredData);
-  };
+//      // Rebuild the table using the filtered data
+//     // @NOTE: If no date was entered, then filteredData will
+//     // just be the original tableData.
+//     buildTable(filteredData);
+//   };
 
-//listen for event to run fx
-d3.selectAll("#filter-btn").on("click", handleClick);
+// //listen for event to run fx
+// d3.selectAll("#filter-btn").on("click", handleClick);
 
-buildTable(tableData);
+// buildTable(tableData);
 
 
 //
@@ -101,20 +138,21 @@ function handleClick() {
   
   // Attach an event to listen for the form button
   d3.selectAll("#filter-btn").on("click", handleClick);
-  detects change
+//  detects change
+
   // Build the table when the page loads
   buildTable(tableData);
 
-  if _change was made___ 
-  update_filters()
+//   if _change was made___ 
+//   update_filters()
 
 
-3.  updateFilters()
-  variabled3select
+// 3.  updateFilters()
+//   variabled3select
 
-4a  d3.select(this)
-  variable 
+// 4a  d3.select(this)
+//   variable 
 
-4b d3.select(this) save value of car
+// 4b d3.select(this) save value of car
    
- 4c. d3.select(this) 
+//  4c. d3.select(this) 
